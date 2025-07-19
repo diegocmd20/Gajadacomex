7709b87d181b375eb5afa6757fd7ec8e7a29014a
diegocmd20 Home <diegocmd20@gmail.com>
2025-07-19 17:45:00 -0400

    Navbar modified

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light navbar-custom fixed-top" :class="{ 'shadow-sm': scrolledNav }">
+        <div class="container">
+            <a class="navbar-brand" href="#">Advantrade</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="#hero">Inicio</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#nosotros">Nosotros</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#servicios">Servicios</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#contacto">Contacto</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    props: ['scrolledNav']
+};
+</script>
