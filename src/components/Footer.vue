892526538dc1b748768338fa5e426d6b664e4fda
diegocmd20 Home <diegocmd20@gmail.com>
2025-07-19 18:04:09 -0400

    Footer.vue modified

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <footer class="py-4 bg-dark text-white text-center">
+        <div class="container">
+            <small>&copy; 2025 Advantrade. Todos los derechos reservados.</small>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+};
+</script>
