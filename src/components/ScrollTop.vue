378a0eac29c1d451b7dd069443958437e75b27cd
diegocmd20 Home <diegocmd20@gmail.com>
2025-07-19 17:56:48 -0400

    ScrollTop.vue modified

---
+++ src/components/ScrollTop.vue
@@ -0,0 +1,12 @@
+<template>
+    <button v-if="scrollTop" @click="$emit('scroll-to-top')" class="scroll-top">
+    ↑
+  </button>
+</template>
+
+<script>
+export default {
+    name: 'ScrollTop',
+    props: ['scrollTop']
+};
+</script>
