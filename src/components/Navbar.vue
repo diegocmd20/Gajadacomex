ec9c9d9cfae2e9cb7c4a7c3f83ed86af92ed89b4
diegocmd20 Home <diegocmd20@gmail.com>
2025-07-20 19:39:21 -0400

    Navbar modified

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,14 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-light navbar-custom fixed-top" :class="{ 'shadow-sm': scrolledNav }">
         <div class="container">
-            <a class="navbar-brand" href="#">Advantrade</a>
+            <a class="navbar-brand" href="#">Gajadacomex</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link" href="#hero">Inicio</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#nosotros">Nosotros</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#inicio">Inicio</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#nosotros">Quiénes Somos</a></li>
                     <li class="nav-item"><a class="nav-link" href="#servicios">Servicios</a></li>
                     <li class="nav-item"><a class="nav-link" href="#contacto">Contacto</a></li>
                 </ul>
