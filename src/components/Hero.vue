f9c2744b91f4c6b44a01a9c00cd51d758f1d7fd3
diegocmd20 Home <diegocmd20@gmail.com>
2025-07-20 19:43:57 -0400

    Text added in Hero.vue

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,8 +1,9 @@
 <template>
     <section class="hero-section">
         <div class="container">
-            <h1 class="display-4">Bienvenido a Advantrade</h1>
-            <p class="lead">Impulsamos la transformación digital de tu negocio.</p>
+            <h1 class="display-4">GAJADACOMEX</h1>
+            <h2 class="display-5">Expertos en comercio internacional</h2>
+            <p class="lead">Simplificamos los procesos aduanales para que tu negocio crezca sin fronteras</p>
         </div>
     </section>
 </template>
