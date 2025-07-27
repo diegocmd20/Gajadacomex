e238d40a36cc58a7bb93fd8323b2a807435ccf4e
diegocmd20 Home <diegocmd20@gmail.com>
2025-07-27 19:12:33 -0400

    custom.css added, Hero section modified, Vue default styles hidden

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,9 +1,9 @@
 <template>
     <section class="hero-section">
-        <div class="container">
-            <h1 class="display-4">GAJADACOMEX</h1>
-            <h2 class="display-5">Nos adaptamos a tus necesidades</h2>
-            <p class="lead">Tu Aliado Estratégico en Logística y Comercio Exterior</p>
+        <div class="container text-center py-5">
+            <h1 class="display-4 text-white">GAJADACOMEX</h1>
+            <h2 class="display-5 text-white">Nos adaptamos a tus necesidades</h2>
+            <p class="lead text-white">Tu Aliado Estratégico en Logística y Comercio Exterior</p>
         </div>
     </section>
 </template>
