6ddbcecf15f92ef0b5f7c24f2febcabe14b7a5db
diegocmd20 Home <diegocmd20@gmail.com>
2025-08-01 22:29:50 -0400

    Cards animation added

--- src/components/ServiciosPreview.vue
+++ src/components/ServiciosPreview.vue
@@ -4,12 +4,14 @@
             <h2 class="mb-4 text-center">Nuestros Servicios</h2>
             <div class="row">
                 <h4 class="mb-4">Eficiencia y Transparencia en Cada Paso de tu Carga</h4>
-                <p>Entendemos que cada operación de comercio exterior es única y requiere un manejo preciso y continuo. En GAJADACOMEX, te ofrecemos información específica y detallada en todo momento, garantizando claridad, confianza y respuesta rápida a tus necesidades.</p>
+                <p>Entendemos que cada operación de comercio exterior es única y requiere un manejo preciso y continuo.
+                    En GAJADACOMEX, te ofrecemos información específica y detallada en todo momento, garantizando
+                    claridad, confianza y respuesta rápida a tus necesidades.</p>
                 <div v-for="(servicio, index) in serviciosPrincipales" :key="index" class="col-md-4 mb-4">
-                    <div class="card h-100" data-aos="fade-up">
+                    <div class="card h-100 p-2 services-card" data-aos="fade-up">
                         <div class="card-body text-center">
                             <i :class="[servicio.icono]"></i>
-                            <h5 class="card-title">{{ servicio.titulo }}</h5>
+                            <h5 class="card-title my-3">{{ servicio.titulo }}</h5>
                             <p class="card-text">{{ servicio.descripcion }}</p>
                         </div>
                     </div>
